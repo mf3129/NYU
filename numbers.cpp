cb24803b335ca645a623bf70e2092da98cc5ab36
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-03-11 23:33:24 -0400

    7 Functions

--- numbers.cpp
+++ numbers.cpp
@@ -2,9 +2,9 @@
  Author: Makan Fofana
  Course: CSCI-136
  Instructor: ]]
- Assignment: Lab5A
+ Assignment: Lab5G
  
- Asks the user to input two integer numbers and print Yes if the first number is divisible by the second, otherwise print No. It should return true if n is a prime, otherwise return false. Change the main function to test your new code.
+ Asks the user to returns the number of prime numbers in the interval a ≤ x ≤ b. Change the main function to test the new code.
 */
 
 #include <iostream>
@@ -48,6 +48,7 @@ bool isPrime(int n) { // Prime number determination
     
 }
 
+
 int nextPrime(int n){
     
     int newValue = 0;
@@ -65,17 +66,99 @@ int nextPrime(int n){
 }
 
 
+int countPrimes(int a, int b) {
+    
+    int countingPrime = 0;
+    int countingValue = 0;
+    
+    for(int i = a; i <= b ; i++) {
+        if(isPrime(i)){
+            countingValue = nextPrime(i);
+            //cout << "Counting Value" << countingValue << endl;
+            countingPrime++;
+        } else {
+        }
+    }
+    
+    
+//    int newValue = 0;
+//    int countingPrime = 0;
+//
+//    for (int i = 1; i <= b ; i++) {
+//
+//
+//        newValue = a + i;
+//        if (isPrime(newValue)) {
+//            countingPrime++;
+//        }
+//    }
+    
+return countingPrime;
+    
+}
+
+bool isTwinPrime(int n) {
+    
+    if (isPrime(n)) {
+        if(isPrime(n + 2) == true || isPrime(n - 2) == true){
+            return true;
+        } else {
+            return false;
+            }
+    } else {
+            return false;
+    }
+    
+    
+}
+
+int nextTwinPrime(int n) {
+    
+    int newValue = 0;
+    bool newPrime = false;
+    int x = 10000;
+    
+    
+    for (int i = 1; i < x ; i++) {
+        
+
+        newPrime = isTwinPrime(n + i);
+        
+        if(newPrime == true){
+            newValue = n + i;
+            cout << "This is the smallest value: " << newValue << endl;
+            return newValue;
+        }
+        
+    }
+    
+    return newValue;
+}
+
+int largestTwinPrime(int a, int b){
+    
+
+    for (int i = b; i >= a; i--) {
+        if((isPrime(i) && isPrime(i+2)) || (isPrime(i) && isPrime(i-2))){
+            return i;
+        }
+    }
+            return -1;
+        
+    }
+
+
 
 int main() {
-    int a = 0;
-    int b = 0;
-
-    cout << "Enter value 1: " << endl;
-    cin >> a;
-
+//    int a = 0;
+//    int b = 0;
+//
+//    cout << "Enter value 1: " << endl;
+//    cin >> a;
+//
 //    cout << "Enter value 2: " << endl;
 //    cin >> b;
-//
+////
 //    //    if(isDivisibleBy(a,b)){ //Printing Yes or No
 //    //        cout<<"Yes"<<endl;
 //    //    }
@@ -91,7 +174,11 @@ int main() {
 //
 //    }
 
-   cout << "Here it is " << nextPrime(a) << endl;
+   cout <<  largestTwinPrime(5, 12) << endl;
+   cout <<  largestTwinPrime(3, 36) << endl;
+   cout <<  largestTwinPrime(15, 29) << endl;
+   cout <<  largestTwinPrime(10, 27) << endl;
+
     
     
     return 0;
