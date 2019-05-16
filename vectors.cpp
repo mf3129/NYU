0c8248f8402847eaf6cc729208f5a78d54ec2ef6
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-05-16 09:24:29 -0400

    vector template personal review

---
+++ vectors.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  lab
+//
+//  Created by Makan fofana on 5/13/19.
+//  Copyright © 2019 Makan fofana. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> makeVector(int n)
+{
+    vector<int> result;
+    for (int i = 0; i < n ; i++) {
+        result.push_back(i);
+    }
+    return result;
+};
+
+
+
+
+int main() {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
